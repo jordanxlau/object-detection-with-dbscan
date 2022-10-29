84e1a349b6b1479ab0bcf2a54ee4174959921f4f
jordanxlau <jordanxlau@yahoo.ca>
2022-10-29 13:18:59 -0400

    created pushAll and save methods

--- DBScan.java
+++ DBScan.java
@@ -37,7 +37,9 @@ public class DBScan{
 
     /* pushes all elements of list, neighbours into stack, stack */
     private static void pushAll(Stack stack, NearestNeighbours neighbours){
-
+        while(Point3D p : neighbours.db){
+            stack.push(p);
+        }
     }
     
     //minPts: The minimum number of points (a threshold) in the neighborhood of a point for this one to be considered to belong to a dense region.
@@ -75,10 +77,9 @@ public class DBScan{
     public static List<Point3D> read(String filename){
         String input;
         double x, y, z;
-        List<Point3D> db = new ArrayList<Point3D>();
 
         try {
-            BufferedReader r = new BufferedReader(new FileReader("Input.csv"));
+            BufferedReader r = new BufferedReader(new FileReader(filename));
             input = r.readLine();
             while ((input = r.readLine()) != null) {
                 String[] array = input.split(",");
@@ -87,7 +88,7 @@ public class DBScan{
                 y = Double.valueOf(array[1]);
                 z = Double.valueOf(array[2]);
 
-                db.add( new Point3D(x, y, z) );
+                this.db.add( new Point3D(x, y, z) );
             }
 
             r.close();
@@ -97,22 +98,36 @@ public class DBScan{
     }
 
     public void save(String filename){
+        String input;
+        double x, y, z;
+        List<Point3D> db = new ArrayList<Point3D>();
 
+        try {
+            BufferedWriter w = new BufferedWriter(new FileWriter(filename));
+            w.write("x,y,z,C,R,G,B");
+            while (this.db.size() > 0) {
+                Point3D p = db.pop();
+                w.write( p.getX() + ", " + p.getY() + ", " + p.getZ() + ", " p.getClusterLabel() );
+            }
+
+            w.close();
+        } catch (IOException e) {}
+
+        return db;
     }
 
     public static void main(String[] args){
-        /*args = {}
-
         String filename = args[0];
-        int eps = Integer.valueOf(args[1]);
-        int minPts = Integer.valueOf(args[2]);
+        double eps = Double.valueOf(args[1]);
+        double minPts = Double.valueOf(args[2]);
 
-        System.out.println("TRACE:");
-        System.out.println("filename, eps, minPts: " + filename + eps + minPts);
+        System.out.println("TRACE: filename eps minPts: " + filename + " " + eps + " " + minPts);
 
-        DBScan scene = new DBScan(read(filename));
+        DBScan scene = new DBScan(new ArrayList<Point3D>());
         scene.setEps(eps);
-        scene.setMinPts(minPts);*/
+        scene.setMinPts(minPts);
+        scene.findClusters(read(filename), eps, minPts);
+        scene.save(filename.substring(0, filename.length() - 4) + "_clusters.csv");
     }
 
 }

29a18f9cc5f2573411edc2004b9ff643191be140
jordanxlau <jordanxlau@yahoo.ca>
2022-12-03 21:57:24 -0500

    minor edits

--- view_clusters.py
+++ view_clusters.py
@@ -2,7 +2,7 @@ import pandas as pd
 import open3d as o3d
 import numpy as np
 
-filename = "Point_Cloud_1_0.6_500.0_967_clusters"
+filename = "Point_Cloud_1_7.0_40.0_8_clusters"
 
 #Get raw point cloud:
 df_Raw_Point_Cloud = pd.read_csv(filename + ".csv")
